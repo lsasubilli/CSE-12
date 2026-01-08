847ee51228371d509b46e7f6fb13d63099c526ff
Lalith Sasubilli <68266855+lsasubilli@users.noreply.github.com>
2026-01-07 23:52:11 -0800

    Implement GenericWorksheet class with basic methods

---
+++ GenericWorksheet.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+
+public class GenericWorksheet<E extends Comparable<E>> {
+    public E[] data;
+
+    public GenericWorksheet() {
+        this.data = null;
+    }
+
+    public GenericWorksheet(E[] data) {
+        this.data = Arrays.copyOf(data, data.length);
+    }
+
+    public E findMedian() {
+        for (int i = 0; i < data.length; i++) {
+            for (int j = 0; j < data.length - 1; j++) {
+                if (data[j].compareTo(data[j + 1]) > 0) {
+                    int temp = data[j];
+                    data[j] = data[j + 1];
+                    data[j + 1] = temp;
+                }
+            }
+        }
+        int n = arr.length;
+
+        return data[data.length / 2];
+    }
+
+    public E findLast() {
+        return data[data.length - 1];
+    }
+
+    public E removeFirst() {
+        E[] newArr = (E[]) new Object[data.length - 1];
+        for (int i = 1; i < newArr.length; i++) {
+            newArr[i - 1] = data[i];
+        }
+        return newArr;
+    }
+
+    @Override
+    public String toString() {
+        String str = "";
+        for (int i = 0; i < data.length; i++) {
+            str += data[i];
+        }
+        return str;
+    }
+}
